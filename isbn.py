14a660b9058c5a4b460eaef7bade21490561d9cb
pranav435 <pranavsavla2003@gmail.com>
2020-09-01 15:48:18 +0530

    First commit. All code files can be found

---
+++ isbn.py
@@ -0,0 +1,69 @@
+#the following function, when given an ISBN number, that is, a positive integer which is 13 digits long, will check its validity using the following algorithm
+#algorithm:
+"""
+Step 1. Add up all the odd_positioned digits. That is: odd_total=isbn[0]+isbn[2]+isbn[4]+isbn[6]+isbn[8]+isbn[10]+isbn[12]
+Step 2: Add up all the evenly positioned digits, that is, even_total=isbn[1]+isbn[3]+isbn[5]+isbn[7]+isbn[9]+isbn[11], and multiply the result by 3: even_total*=3
+Step 3: Add the totals from steps 1 and 2: total=odd_total+even_total
+step 4: If the remainder between the total and 10 is 0, then the number is valid. Else, it is not: if total%10==0: True. Else: False.
+This happens because the last digit in the number is a check digit. Each ISBN number has a unique check digit that identifies its validity. If the remainder is not 0, then the check digit is invalid, and thus, the ISBN number is invalid.
+"""
+#Note: the function isbn_check(isbn_number), is only a module. You can import it into your program by saying import isbn or from isbn import isbn_check.
+"""
+isbn_check
+Input: an ISBN number, that is, a 13 digit non-negative number.
+Output: 1 if the number is valid, 0 if it is not.
+Error codes:
+-1: Negative number.
+-2: Number too short or too long.
+"""
+
+def isbn_check(isbn_number):
+
+#the following few lines are declarations of variables that our function will then use.
+	isbn=[] #the list on which the main validity check of the ISBN number will take place
+	odd_total=0 #an oxiliary variable that will help in the validity check
+	even_total=0 #another oxiliary variable
+	total=0 # a third oxiliary or helper variable that will help in step 3 of the algorithm.
+	isbn_string=str(isbn_number) # this variable will be useful in setting up the above isbn list for the operation, but will not have any other use.
+	i=0 # this is our itteration variable throughout the function
+
+#the following few conditionals perform tests to seee if the number passed as a parameter is in the format of an ISBN number, that is a 13 digit positive integer.	
+	if isbn_number<=0: # this checks to see if the number is a positive integer
+		return -1 # our error code.
+
+	if len(isbn_string)<13 or len(isbn_string)>13: # this checks the length of the number, which has to be 13 characters
+		return -2 # error code
+
+#Converting the stringified isbn number to a list of strings
+	for char in isbn_string:
+		isbn.append(char)
+
+#the following snippet converts our list into a list of integers, which can then be processed to check validity.
+	i=0
+	while i<13:
+		isbn[i]=int(isbn[i])
+		i+=1
+#the following loops add up the numbers, all except the check digit.
+#first, i is initialized to 0
+	i=0
+	while i<13:
+		odd_total+=isbn[i]
+		i+=2
+
+#re-initializing the variable i to 1, as we are now adding up even numbers
+	i=1
+	while i<=12:
+		even_total+=isbn[i]
+		i+=2
+
+# the following line makes a change to our even_total variable so that the processing can be done right
+	even_total*=3
+
+#the following line adds up both the totals
+	total=odd_total+even_total
+
+#now, the final check is implemented
+	if total%10==0:
+		return 1
+	else:
+		return 0
---
+++ isbn_test.py
@@ -0,0 +1,61 @@
+"""
+This program tests our isbn_check module. The code for that function can be found in the isbn.py file attached.
+This program will also show exception handling and importing.
+To get the isbn_check function, we have to import it.
+We can do that in two ways.
+Note, however, that we have to import it in the first few lines of our code. The length of the import section depends upon how many modules are being imported.
+To import, we can either write
+import isbn
+Thus, the whole isbn module will be imported. That means that whatever variables, functions, classes, statements and code written in isbn.py will be accessible to this current file.
+We can also be more specific and write
+from isbn import isbn_check
+This will only import the data in the isbn_check function in the isbn.py file. This is useful if we want to import one function from a huge code file with many functions, classes and variable definitions.
+Note that in this example, I usually like to use the first method of immporting, and that is what will be used here. Feel free to play around with the code and modify it to use the second one.
+"""
+
+import isbn # we import our module
+#now we declare a few global variables
+result=0 # this stores the end-result
+correct=False # this is a flag that we will use shortly, when we take the input from the user. Keep reading to find out how it is used.
+isbn_input=0 # this is the variable that will hold the user's input
+# now we define a loop in which we take the isbn number from the user. We put it in a loop so that, if an error were to occur, it would show the appropriate message and let the user try again.
+while correct!=True: #here's where the correct flag is used. We say correct!=True, which means that the value of correct can be anything but True.
+	try: #this is the main try block. This is the first test that we perform. The reason we have a try block is that we can figure out if the user has entered a string and not have the program display a traceback error, which is not user-friendly, but more importantly, a huge security rik.
+		isbn_input=int(input("Enter an ISBN number: ")) #we take the input from the user
+	except: # this block will define what the program should do if an exception occurs.
+		print("Oops. Invalid input. Please enter an ISBN number, which is an integer.") #this is our error message
+		continue #we let the user try again
+#the following lines of code will only execute if the user enters an integer.
+#the following tests will ensure that the user enters a number in the ISBN format, which is a 13 digit positive integer.
+#to do this, we first convert the integer into a string. We could use the error codes, but that is a work in progress as of the time of writing, and is yet to be refined.
+
+	if isbn_input<0: #the number is not positive.
+		print("Oops. The ISBN number is not a positive integer. Please enter a positive integer and try again.") #appropriate error message
+		continue #we let the user try again
+#the following lines will only execute  if the above if condition has completed.
+	isbn_input=str(isbn_input)
+	if len(isbn_input)<13 or len(isbn_input) > 13: #number too short or too long
+		print("Oops. The number is not of the ISBN format, which is a 13 digit positive number. Please check the length of the ISBN number and try again.") #error message
+		continue #we let the user try again
+#the following line only executes if all the above conditions return false, or if the user has entered a valid ISBN number.
+	correct=True #we exit the loop
+
+#now we run the input through the function. But first, we have to convert it back to an int
+isbn_input=int(isbn_input) #casts it back
+result=isbn.isbn_check(isbn_input) #now we run the isbn_input variable through the function to check the validity.
+#the final part of our venture is to print the appropriate messages.
+if result==1: #the number is valid
+	print("Congratulations! The ISBN number is Valid!") #appropriate message
+else:
+	print("Oops. The number is not valid. Please check the ISBN number and try again.") #appropriate error message
+
+"""
+Final Notes:
+Congratulations!
+We have come to our venture. We now have a working and well oiled implementation of our algorithhm.
+Feel free to play around with the code and try to change things about.
+If in doubt, don't forget to E-Mail me at
+pranavsavla2003@gmail.com
+to discuss the problem.
+Thanks for staying with me!
+"""
